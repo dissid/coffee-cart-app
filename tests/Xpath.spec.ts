6a4abc00efd085f274f68a0366f79a4ccabc3f3f
dmytro.sydorenko <dissid90@gmail.com>
2026-09-26 17:45:24 +0300

    refactor

--- tests/Xpath.spec.ts
+++ tests/Xpath.spec.ts
@@ -8,20 +8,17 @@ test("Select rows via checkboxes", async ({ page }) => {
   const checkboxes = page.locator("//*[contains(@data-testid,'interactions-row-select')]");
   await expect(checkboxes.first()).toBeVisible();
   const checkboxCount = await checkboxes.count();
-  console.log(checkboxCount);
 
   for (let i = 0; i < checkboxCount; i++) {
     await checkboxes.nth(i).check();
-    await expect(page.locator("//*[@data-testid='interactions-selected-count']")).toHaveText(
-      "Вибрано: " + checkboxCount,
-    );
+    await expect(page.locator("//*[@data-testid='interactions-selected-count']")).toHaveText("Вибрано: " + (i + 1));
   }
 });
 
 test("Sort the table by status", async ({ page }) => {
   await page.locator("//*[@data-testid='interactions-sort-status']").click();
-  await expect(page.locator("//*[@data-testid='interactions-table-row-2']//*[text()='Failed']")).toBeVisible();
+  await expect(page.locator("//tr").nth(1).locator("//*[text()='Failed']")).toBeVisible();
 
   await page.locator("//*[@data-testid='interactions-sort-status']").click();
-  await expect(page.locator("//*[@data-testid='interactions-table-row-2']//*[text()='Skipped']")).toBeVisible();
+  await expect(page.locator("//tr").nth(1).locator("//*[text()='Skipped']")).toBeVisible();
 });
